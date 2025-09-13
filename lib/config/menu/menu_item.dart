01d62b2d0f8df160cf28674e899ff9f46eb07974
andre.neri <andre.neri@saude.ba.gov.br>
2025-09-13 19:23:57 -0300

    Home Screen criado

---
+++ lib/config/menu/menu_item.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart' show IconData, Icons;
+
+class MenuItem {
+  final String title;
+  final String subtitle;
+  final String link;
+  final IconData icon;
+
+  const MenuItem({
+    required this.title,
+    required this.subtitle,
+    required this.link,
+    required this.icon,
+  });
+}
+
+const MunuItems = <MenuItem>[
+  MenuItem(title: 'Botões', subtitle: 'Alguns Botões de Flutter', link: '/buttons', icon: Icons.smart_button_outlined),
+  MenuItem(title: 'Cardes', subtitle: 'Um Card de Flutter', link: '/cards', icon: Icons.credit_card),
+];
---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+const colorLists = <Color>[
+  Colors.blue,
+  Colors.red,
+  Colors.pinkAccent,
+  Colors.orange,
+  Colors.yellow,
+  Colors.green,
+  Colors.teal,
+  Colors.blueGrey,
+];
+
+class AppTheme {
+  final int selectedColor;
+
+  AppTheme({this.selectedColor = 0})
+    : assert(selectedColor >= 0, 'Seleceted color must be greater then 0'),
+      assert(
+        selectedColor < colorLists.length,
+        'Selected color must be less equal than ${colorLists.length - 1}',
+      );
+
+  ThemeData getTheme() => ThemeData(
+    colorSchemeSeed: colorLists[selectedColor],
+    appBarTheme: const AppBarTheme(centerTitle: false),
+  );
+}
